c8aa4a1c31e78f8f55edc4999062471b0cedee76
Ruben Baskaran <rbaskaran92@gmail.com>
2020-02-23 01:03:56 +0100

    added RESTFUL routes and description property

--- app.js
+++ app.js
@@ -11,9 +11,28 @@ mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useUn
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 var campground = mongoose.model("Campground", campgroundSchema);
+
+// campground.create(
+//     {
+//         name: "Hollywood camp",
+//         image: "https://www.photosforclass.com/download/pixabay-2023404?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F54e0d7404e52a814f6da8c7dda793f7f1636dfe2564c704c7d2d78d59048cc5c_960.jpg&user=Daria-Yakovleva",
+//         description: "This is the hollywood camp in Los Angeles, CA"
+//     }, function (error, newlyCreated)
+//     {
+//         if (error)
+//         {
+//             console.log(error);
+//         }
+//         else
+//         {
+//             console.log("New campground created!");
+//             console.log(newlyCreated);
+//         }
+//     });
 // SCHEMA SETUP
 
 app.get("/", function (req, res)
@@ -39,7 +58,8 @@ app.post("/campgrounds", function (req, res)
 {
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = { name: name, image: image };
+    var description = req.body.description;
+    var newCampground = { name: name, image: image, description: description };
 
     campground.create(newCampground, function (error, newlyCreated)
     {
@@ -58,6 +78,23 @@ app.get("/campgrounds/new", function (req, res)
 {
     // Shows form that calls POST /campgrounds when submitted
     res.render("new");
+});
+
+app.get("/campgrounds/:id", function (req, res)
+{
+    campground.findById(req.params.id, function (error, foundCampground)
+    {
+        if (error)
+        {
+            console.log(error);
+        }
+        else
+        {
+            console.log("Found campground");
+            console.log(foundCampground);
+            res.render("show", { campground: foundCampground });
+        }
+    });    
 });
 
 app.listen(3000, function ()
