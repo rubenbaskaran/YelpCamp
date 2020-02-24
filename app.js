1691b466e1df2270bc667e6e1f334e31f0a9f0f3
Ruben Baskaran <rbaskaran92@gmail.com>
2020-02-24 11:49:50 +0100

    moved campgroundSchema to separate file

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-
+var campground = require("./models/campground");
 var app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
-
-// SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-var campground = mongoose.model("Campground", campgroundSchema);
-
-// campground.create(
-//     {
-//         name: "Hollywood camp",
-//         image: "https://www.photosforclass.com/download/pixabay-2023404?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F54e0d7404e52a814f6da8c7dda793f7f1636dfe2564c704c7d2d78d59048cc5c_960.jpg&user=Daria-Yakovleva",
-//         description: "This is the hollywood camp in Los Angeles, CA"
-//     }, function (error, newlyCreated)
-//     {
-//         if (error)
-//         {
-//             console.log(error);
-//         }
-//         else
-//         {
-//             console.log("New campground created!");
-//             console.log(newlyCreated);
-//         }
-//     });
-// SCHEMA SETUP
 
 app.get("/", function (req, res)
 {
---
+++ models/campground.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
+
+// campground.create(
+//     {
+//         name: "Hollywood camp",
+//         image: "https://www.photosforclass.com/download/pixabay-2023404?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F54e0d7404e52a814f6da8c7dda793f7f1636dfe2564c704c7d2d78d59048cc5c_960.jpg&user=Daria-Yakovleva",
+//         description: "This is the hollywood camp in Los Angeles, CA"
+//     }, function (error, newlyCreated)
+//     {
+//         if (error)
+//         {
+//             console.log(error);
+//         }
+//         else
+//         {
+//             console.log("New campground created!");
+//             console.log(newlyCreated);
+//         }
+//     });
