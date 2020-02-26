3006b7c9688e142219270c6c3ea1938a1a637eca
Ruben Baskaran <rbaskaran92@gmail.com>
2020-02-26 19:52:54 +0100

    added edit feature to comments

--- routes/comments.js
+++ routes/comments.js
@@ -42,12 +42,44 @@ router.post("/campgrounds/:id/comments", isLoggedIn, function (req, res)
                     comment.author.username = req.user.username;
                     comment.save();
                     campground.comments.push(comment);
-                    campground.save();                        
+                    campground.save();
                     res.redirect("/campgrounds/" + campground._id);
                 }
             })
         }
     })
+});
+
+// Show "edit comment" view
+router.get("/campgrounds/:id/comments/:comment_id/edit", function (req, res)
+{
+    Comment.findById(req.params.comment_id, function (err, foundComment)
+    {
+        if (err)
+        {
+            res.redirect("back");
+        }
+        else
+        {
+            res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
+        }
+    })
+});
+
+// Save comment changes to database
+router.put("/campgrounds/:id/comments/:comment_id", function (req, res)
+{
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (error, updatedComment)
+    {
+        if (error)
+        {
+            res.redirect("back");
+        }
+        else
+        {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
 });
 
 // Middleware for checking authorization
